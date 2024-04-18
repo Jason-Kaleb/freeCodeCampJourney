efa08aa2960e7214ca74d724cbc3e8a2abc62bd6
Jason <kyleskaleb56@gmail.com>
2024-04-18 22:01:58 +0200

    updated HTML and JS files and Completed project

--- javascriptDSA/calorieCounter/script.js
+++ javascriptDSA/calorieCounter/script.js
@@ -31,24 +31,6 @@ function addEntry()
     targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
-function getCaloriesFromInputs(list)
-{
-    let calories = 0;
-    for (const item of list)
-    {
-        const currVal = cleanInputString(item.value);
-        const invalidInputMatch = isInvalidInput(currVal);
-        if (invalidInputMatch)
-        {
-            alert(`Invalid Input: ${invalidInputMatch[0]}`);
-            isError = true;
-            return null;
-        }
-        calories += Number(currVal);
-    }
-    return calories;
-}
-
 function calculateCalories(e)
 {
     e.preventDefault();
@@ -76,7 +58,51 @@ function calculateCalories(e)
     const remainingCalories = (budgetCalories - consumedCalories) + exerciseCalories;
     const surplusOrDeficit =  (remainingCalories < 0) ? "Surplus" : "Deficit";
 
-    output.innerHTML = ``;
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+
+    output.classList.remove('hide');
+}
+
+function getCaloriesFromInputs(list)
+{
+    let calories = 0;
+
+    for (const item of list)
+    {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch)
+        {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+        calories += Number(currVal);
+    }
+    return calories;
+}
+
+function clearForm()
+{
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+    for (const   container of inputContainers)
+    {
+        container.innerHTML = "";
+    }
+
+    budgetNumberInput.value = '';
+    output.innerText = '';
+    output.classList.add('hide');
 }
 
 addEntryButton.addEventListener('click', addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click', clearForm);

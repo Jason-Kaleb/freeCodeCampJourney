aa78c7abe8f77625e2b390c9e62d90042ff8791b
Jason <kyleskaleb56@gmail.com>
2024-05-15 20:16:07 +0200

    updated script.js

--- javascriptDSA/dateFormatter/script.js
+++ javascriptDSA/dateFormatter/script.js
@@ -4,3 +4,27 @@ const dateOptionsSelectElement = document.getElementById('date-options');
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const formattedDate = `${day}-${month}-${year}`;
+
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener('change', (event) => {
+    switch (dateOptionsSelectElement.value)
+    {
+        case "yyyy-mm-dd" :
+            currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-");
+        break;
+        case "dd-mm-yyyy" :
+            currentDateParagraph.textContent = formattedDate;
+        break;
+        case "mm-dd-yyyy-h-mm" :
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+        break;
+        default :
+            currentDateParagraph.textContent = formattedDate;
+        break;
+    }
+});
